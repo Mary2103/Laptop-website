60f01c587e9f39356353ca836eeec15551564127
Chau Nhi <chaunhihue@gmail.com>
2023-04-12 20:38:14 +0700

    upload code

---
+++ script.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function () {
+
+     const slider = document.querySelector(".home__main-banner--slider");
+
+     const sliderMain = document.querySelector(".slider-main");
+
+     const sliderItems = document.querySelector(".slider-item");
+
+     const nextBtn = document.querySelector(".slider-next");
+
+     const prevBtn = document.querySelector(".slider-prev");
+
+     const dotItem = document.querySelector(".slider-dot-item");
+
+     const sliderItemWidth = sliderItems[0].offsetWidth;
+
+     const sliderLength = sliderItems.length;
+
+     nextBtn.addEventListener("click", function () {
+
+     });
+
+});
